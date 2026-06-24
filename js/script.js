7a9bda9481991601666cac4f0907983105ff9a72
andriashikwe-gif <andria.shikwe@strathmore.edu>
2026-06-24 13:52:29 +0300

    Updated css

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+if (darkModeBtn) {
+
+    darkModeBtn.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            darkModeBtn.textContent = "☀️ Light Mode";
+        } else {
+            darkModeBtn.textContent = "🌙 Dark Mode";
+        }
+
+    });
+
+}
+
+if (document.body.classList.contains("dark-mode")) {
+    darkModeBtn.textContent = "☀️ Light Mode";
+    darkModeBtn.classList.remove("btn-dark");
+    darkModeBtn.classList.add("btn-warning");
+} else {
+    darkModeBtn.textContent = "🌙 Dark Mode";
+    darkModeBtn.classList.remove("btn-warning");
+    darkModeBtn.classList.add("btn-dark");
+}
